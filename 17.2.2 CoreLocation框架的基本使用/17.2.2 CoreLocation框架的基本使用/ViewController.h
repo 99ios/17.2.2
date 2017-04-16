37acdac4066365b9f27d7c0d1d0f82293d94eb25
test <422538722@qq.com>
2017-04-16 16:54:26 +0800

    17.2.2
    CoreLocation框架的基本使用

---
+++ 17.2.2 CoreLocation框架的基本使用/17.2.2 CoreLocation框架的基本使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  17.2.2 CoreLocation框架的基本使用
+//
+//  Created by 李维佳 on 2017/4/12.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 17.2.2 CoreLocation框架的基本使用/17.2.2 CoreLocation框架的基本使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  17.2.2 CoreLocation框架的基本使用
+//
+//  Created by 李维佳 on 2017/4/12.
+//  Copyright © 2017年 Liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
